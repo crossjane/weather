ba1c8681b009fd8b0fecab6cef65ed226d799ec5
crossjane <cross.jane.kim@gmail.com>
2025-02-14 16:53:27 +0900

    25.02.14 수정중

--- src/App.js
+++ src/App.js
@@ -1,49 +1,83 @@
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   //동적인 상태 ? 날씨, 온도 , 날씨 description 은 어떻게 생각하지 ? 
-  const [weather, setWeather] = useState("");
-  const [temperature, setTemperature] = useState("");
-  const [weatherDescription, setWeatherDescription] = useState("");
+  // useState상태 날씨 하나만 만들기 객체로 . 
+  const [weather, setWeather] = useState({
+    weatherState:"",
+    description:"",
+    temperature:""
+  });
 
-  const weatherDescriptions = {
-    clear: "맑음",
-    cloudy: "흐림",
-    rain : "비",
-    snow: "눈",
-};
+//   const weatherDescriptions = {
+//     clear: "맑음",
+//     cloudy: "흐림",
+//     rain : "비",
+//     snow: "눈",
+// };
 
 // 날씨 상태 변경 함수
   function changeWeather(weather){
-    setWeather(weather);
-    setWeatherDescription(weatherDescriptions[weather]);
+
+
   }
 
 
 
-
-
-
-  async function getWeatherLatestVersion(){
-    const API_KEY ="";
-
-    const position = await getCurrentPosition();
-    const latitude = position.coords.latitude;
-    const logitude = position.coords.logitude;
-    const url = await fetch();
-    const parsed = await response.json();
-
-    let temp = parsed.main.temp;
-    temp = Math.round(temp) +  "°C";
-
-    
-  
+  function getCurrentPosition(){
+    return new Promise(function(resolve,reject){
+      navigator.geolocation.getCurrentPosition(resolve,reject);
+    });
   }
 
 
+  async function getWeather(){
+      try{
+        const API_KEY="";
 
+        const position = await getCurrentPosition();
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+          const result = await fetch()
+          const parsed = await result.json();
+          // let temp = parsed.main.temp;
+          // temp = Math.round(temp) + "°C";
+
+          console.log(parsed);
+        
+          const copyWeather = {...weather};
+
+          const id = parsed.weather[0].id;
+          if(id >= 200 && id < 600){
+            copyWeather.description="비";
+          }else if(id >= 600 && id < 700){
+            copyWeather.description = "눈";
+          }else if(id >=801){
+            copyWeather.description = "흐림";
+          }else{
+            copyWeather.description = "맑음";
+          }  
+
+          setWeather(copyWeather);
+
+      
+    } catch(error){
+      console.error(error);
+    }
+}
+
+
+
+
+function changeTemperature() {
+  return 1 + "°C";
+}
+
+useEffect(()=>{
+  getWeather();
+},[]);
 
 
   return (
@@ -59,8 +93,9 @@ function App() {
       <div className="weather-container">
         <div className="weather-icon"></div>
         <div className="weather-info">
-          <div className="temperature" ><span id="temperature">-1°C</span></div>
-          <div className="description">흐림</div>
+          {/* 안에 함수를 쓸 수도 있음. changeTemperature */}
+          <div className="temperature" ><span id="temperature">{changeTemperature()}</span></div>
+          <div className="description" >{weather.description}</div>
 
         </div>
       </div>
